718d829885c5e9665aeabcdeef427f2cb1e847fe
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-06-16 23:31:25 +0600

    added style data

---
+++ frontend/styles/data.js
@@ -0,0 +1,47 @@
+
+const screenSize = {
+    mobileSmall: '320px',
+    mobileMedium: '375px',
+    mobileLarge: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptopLarge: '1440px',
+    desktop: '2560px',
+};
+
+const devices = {
+    mobileS: `(max-width: ${screenSize.mobileSmall})`,
+    mobileM: `(max-width: ${screenSize.mobileMedium})`,
+    mobileL: `(max-width: ${screenSize.mobileLarge})`,
+    tablet: `(max-width: ${screenSize.tablet})`,
+    laptop: `(max-width: ${screenSize.laptop})`,
+    laptopL: `(max-width: ${screenSize.laptopLarge})`,
+    desktop: `(max-width: ${screenSize.desktop})`,
+};
+
+
+
+const color = {
+    dark: "#000000",
+    light: "#FFFFFF",
+    primary: "#23272A",
+    primaryMedium: "#2C2F33",
+    secondary: "#5865F2",
+    secondaryMedium: "#404EED",
+    text: {
+        light: "#99AAB5",
+        lighter: "#C0C0C0",
+        lightest: "#FFFFFF",
+        inverted: "#000000"
+    }
+};
+
+
+const data = {
+    styles: {
+        devices: devices,
+        color: color,
+    }
+};
+
+export { data as default }
