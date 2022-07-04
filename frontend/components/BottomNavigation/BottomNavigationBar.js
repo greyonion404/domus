25e42f18540439a4416cc46406a8270111c117a0
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-07-04 11:04:06 +0600

    feature: dynamic navigation based on viewerMode
    added owner and renter navigation in the same component,
    Viewing mode can control which page user visits

--- frontend/components/BottomNavigation/BottomNavigationBar.js
+++ frontend/components/BottomNavigation/BottomNavigationBar.js
@@ -7,108 +7,14 @@ import { RiAddCircleFill } from 'react-icons/ri'
 
 
 import {
-    getPersistantState, 
-    useStorePersistance, 
+    getPersistantState,
+    useStorePersistance,
     useUserPreferencesStore
 } from "../../store";
 import { Text } from "../../styles/Text";
 import { NavbarContainer, NavbarMain, NavLink, centerChilds } from "./BottomNavbar.styles";
 
 
-function OwnerNavigation() {
-
-    const PAGES =
-    {
-        ADD_OWNED_PROPERTY: 'ADD_OWNED_PROPERTY',
-        HOME: 'HOME',
-        NOTIFICATION: 'NOTIFICATION',
-    }
-
-    const Router = useRouter();
-    const [currentPage, setCurrentPage] = useState(PAGES.HOME);
-
-    useEffect(() => {
-        if (!Router.pathname) return;
-        if (Router.pathname.includes('add-owned-property')) setCurrentPage(PAGES.ADD_OWNED_PROPERTY);
-        else if (Router.pathname.includes('owner-complaint-notifications')) setCurrentPage(PAGES.NOTIFICATION);
-        else setCurrentPage(PAGES.HOME);
-    }, [Router.pathname]);
-
-
-    return (
-        <NavbarMain>
-            <NavLink active={(currentPage === PAGES.ADD_OWNED_PROPERTY)} onClick={() => { Router.push('/add-owned-property') }}>
-                <Text size={3} style={centerChilds}>
-                    <RiAddCircleFill />
-                </Text>
-            </NavLink>
-            <NavLink active={(currentPage === PAGES.HOME)} onClick={() => { Router.push('/') }}>
-                <Text size={3} style={centerChilds}>
-                    <FaHome />
-                </Text>
-            </NavLink>
-            <NavLink active={(currentPage === PAGES.NOTIFICATION)} onClick={() => { Router.push('/owner-complaint-notifications') }}>
-                <Text size={3} style={centerChilds}>
-                    <ImNotification />
-                </Text>
-            </NavLink>
-
-            <NavLink onClick={() => { Router.push('/api/auth/logout') }}>
-                <Text size={3} style={centerChilds}>
-                    <AiOutlineLogout />
-                </Text>
-            </NavLink>
-        </NavbarMain>
-    )
-}
-
-function RenterNavigation() {
-
-    const PAGES =
-    {
-        ADD_RENTED_PROPERTY: 'ADD_RENED_PROPERTY',
-        HOME: 'HOME',
-        NOTIFICATION: 'NOTIFICATION',
-    }
-
-    const Router = useRouter();
-    const [currentPage, setCurrentPage] = useState(PAGES.HOME);
-
-    useEffect(() => {
-        if (!Router.pathname) return;
-        if (Router.pathname.includes('add-rented-property')) setCurrentPage(PAGES.ADD_RENTED_PROPERTY);
-        else if (Router.pathname.includes('renter-complaint-notifications')) setCurrentPage(PAGES.NOTIFICATION);
-        else setCurrentPage(PAGES.HOME);
-    }, [Router.pathname]);
-   
-
-
-    return (
-        <NavbarMain>
-              <NavLink active={(currentPage === PAGES.ADD_RENTED_PROPERTY)} onClick={() => { Router.push('/add-rented-property') }}>
-                <Text size={3} style={centerChilds}>
-                    <RiAddCircleFill />
-                </Text>
-            </NavLink>
-            <NavLink active={(currentPage === PAGES.HOME)} onClick={() => { Router.push('/') }}>
-                <Text size={3} style={centerChilds}>
-                    <FaHome />
-                </Text>
-            </NavLink>
-            <NavLink active={(currentPage === PAGES.NOTIFICATION)} onClick={() => { Router.push('/renter-complaint-notifications') }}>
-                <Text size={3} style={centerChilds}>
-                    <ImNotification />
-                </Text>
-            </NavLink>
-
-            <NavLink onClick={() => { Router.push('/api/auth/logout') }}>
-                <Text size={3} style={centerChilds}>
-                    <AiOutlineLogout />
-                </Text>
-            </NavLink>
-        </NavbarMain>
-    )
-}
 
 export default function BottomNavigationBar() {
 
@@ -116,16 +22,85 @@ export default function BottomNavigationBar() {
     const isViewingAsOwner = useUserPreferencesStore((state) => state.isViewingAsOwner);
 
 
+    const PAGES =
+    {
+        ADD_OWNED_PROPERTY: 'ADD_OWNED_PROPERTY',
+        ADD_RENTED_PROPERTY: 'ADD_RENED_PROPERTY',
+        HOME: 'HOME',
+        OWNER_NOTIFICATION: 'OWNER_NOTIFICATION',
+        RENTER_NOTIFICATION: 'RENTER_NOTIFICATION',
+    }
+    const Router = useRouter();
+    const [currentPage, setCurrentPage] = useState(PAGES.HOME);
+
+    useEffect(() => {
+        if (!Router.pathname) return;
+        if (Router.pathname.includes('add-rented-property')) setCurrentPage(PAGES.ADD_RENTED_PROPERTY);
+        else if (Router.pathname.includes('add-owned-property')) setCurrentPage(PAGES.ADD_OWNED_PROPERTY);
+        else if (Router.pathname.includes('renter-complaint-notifications')) setCurrentPage(PAGES.RENTER_NOTIFICATION);
+        else if (Router.pathname.includes('owner-complaint-notifications')) setCurrentPage(PAGES.OWNER_NOTIFICATION);
+        else setCurrentPage(PAGES.HOME);
+    }, [Router.pathname]);
+
+    useEffect(() => {
+        if(isViewingAsOwner && currentPage === PAGES.ADD_RENTED_PROPERTY) Router.push('/add-owned-property');
+        if(!isViewingAsOwner && currentPage === PAGES.ADD_OWNED_PROPERTY) Router.push('/add-rented-property');
+        if(isViewingAsOwner && currentPage === PAGES.RENTER_NOTIFICATION) Router.push('/owner-complaint-notifications');
+        if(!isViewingAsOwner && currentPage === PAGES.OWNER_NOTIFICATION) Router.push('/renter-complaint-notifications');
+
+    }, [isViewingAsOwner]);
+
+
 
     return (
 
         <NavbarContainer>
+            <NavbarMain>
+                {
+                    getPersistantState(hasPersistance, isViewingAsOwner) ?
+                        <NavLink active={(currentPage === PAGES.ADD_OWNED_PROPERTY)} onClick={() => { Router.push('/add-owned-property') }}>
+                            <Text size={3} style={centerChilds}>
+                                <RiAddCircleFill />
+                            </Text>
+                        </NavLink>
+                        :
+                        <NavLink active={(currentPage === PAGES.ADD_RENTED_PROPERTY)} onClick={() => { Router.push('/add-rented-property') }}>
+                            <Text size={3} style={centerChilds}>
+                                <RiAddCircleFill />
+                            </Text>
+                        </NavLink>
+                }
 
-            {
-                getPersistantState(hasPersistance, isViewingAsOwner) ?
-                    <OwnerNavigation /> :  <RenterNavigation />
-            }
 
+                <NavLink active={(currentPage === PAGES.HOME)} onClick={() => { Router.push('/') }}>
+                    <Text size={3} style={centerChilds}>
+                        <FaHome />
+                    </Text>
+                </NavLink>
+
+                {
+                    getPersistantState(hasPersistance, isViewingAsOwner) ?
+                        <NavLink active={(currentPage === PAGES.OWNER_NOTIFICATION)} onClick={() => { Router.push('/owner-complaint-notifications') }}>
+                            <Text size={3} style={centerChilds}>
+                                <ImNotification />
+                            </Text>
+                        </NavLink>
+                        :
+                        <NavLink active={(currentPage === PAGES.RENTER_NOTIFICATION)} onClick={() => { Router.push('/renter-complaint-notifications') }}>
+                            <Text size={3} style={centerChilds}>
+                                <ImNotification />
+                            </Text>
+                        </NavLink>
+                }
+
+
+                <NavLink onClick={() => { Router.push('/api/auth/logout') }}>
+                    <Text size={3} style={centerChilds}>
+                        <AiOutlineLogout />
+                    </Text>
+                </NavLink>
+
+            </NavbarMain>
         </NavbarContainer >
 
     )
