b9321925bbcfa4f9de226e9fef3c7ac409801cdf
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-07-04 08:40:45 +0600

    updated

--- frontend/components/Modals/DeleteOwnedPropertyModal.js
+++ frontend/components/Modals/DeleteOwnedPropertyModal.js
@@ -1,37 +1,37 @@
 import { useRouter } from "next/router";
-import { FaTrash } from "react-icons/fa";
+import { FaTrash, FaUpload } from "react-icons/fa";
 import { centerChilds, Text } from "../../styles/Text";
 import { deleteOwnedPropertyByID } from "../../Utils/database";
 import { DeletePropertyButton, GenericModal } from "./Modals.styles";
 import { useModalStore } from "../../store"
+import { useState } from "react";
 
 export default function DeleteOwnedPropertyModal({ property }) {
     let router = useRouter();
 
-    const toggleIsModalOpen = useModalStore((state) => state.toggleIsModalOpen);
 
+    const toggleIsModalOpen = useModalStore((state) => state.toggleIsModalOpen);
+    const [isDeleting, setIsDeleting] = useState(false);
 
     async function deleteProperty() {
+        setIsDeleting(true);
         const { data, error } = await deleteOwnedPropertyByID(property.propertyID);
-        console.log(data);
-        console.log(error);
-        if (data) {
-            toggleIsModalOpen();
-            window.location.reload(false);
-        }
+        setIsDeleting(false);
+        toggleIsModalOpen();
+        if (data) window.location.reload(false);
+
+
     }
     return (
         <GenericModal style={{ height: "max-content" }}>
             <Text style={centerChilds}>
                 {`Click the button to remove the property @address "`}
-                <span style={{ textDecoration: "underline" }}>
-                    {`${property.address}`}
-                </span>
+                {`${property.address}`}
                 {`" from the list of your owned properties!`}
             </Text>
             <DeletePropertyButton onClick={async () => { await deleteProperty() }}>
                 <Text size={3} style={centerChilds}>
-                    <FaTrash />
+                    {isDeleting ? <FaUpload /> : <FaTrash />}
                 </Text>
             </DeletePropertyButton>
         </GenericModal>
